3e860cebd494c55f60282997daf23911a04e6a26
Leon Bryan <leonard.bryan.email@gmail.com>
2025-05-25 14:37:48 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Document from "./components/Document.jsx"
+
+function App() {
+  return (
+    <>
+      <Document />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Document.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Document() {
+    return (
+        <div class="document-wrapper">
+            <iframe src="https://docs.google.com/document/d/1sfWvAfWoXU23hlb5R_v0SbafAjPPgKAq83D8FFhY6P8/edit?usp=sharing" frameborder="0"></iframe>
+        </div>
+    )
+}
+
+export default Document
